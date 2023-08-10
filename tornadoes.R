60db25391ffb630af97169590a371c3731088973
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-08-10 16:53:24 -0500

    Added maps to tornadoes.R script.

--- tornadoes.R
+++ tornadoes.R
@@ -26,3 +26,22 @@ tornadoes$dayofyear <- paste(tornadoes$MONTH_NAME,
                              sep = "-")
 table(tornadoes$dayofyear)
 plot(table(tornadoes$dayofyear))
+table(tornadoes$STATE)
+sort(table(tornadoes$STATE))
+plot(tornadoes$BEGIN_LON,tornadoes$BEGIN_LAT,pch = 20)
+library(ggplot2)
+map_usa_latlongs <- function (lats, longs) {
+  dat <- data.frame(lat = lats,long = longs)
+  return(ggplot(dat,aes(x = long,y = lat)) +
+         geom_path(data = map_data("state"),
+                   aes(x = long,y = lat,group = group),
+                   color = "gray") +
+         geom_point(shape = 20,size = 1) +
+     	 theme_classic())
+}
+map_usa_latlongs(tornadoes$BEGIN_LAT,tornadoes$BEGIN_LON)
+rows <- which(tornadoes$BEGIN_LON < -140 |
+              tornadoes$BEGIN_LAT < 25)
+tornadoes[rows,]
+tornadoes <- tornadoes[-rows,]
+map_usa_latlongs(tornadoes$BEGIN_LAT,tornadoes$BEGIN_LON)
